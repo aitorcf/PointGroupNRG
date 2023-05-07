61221f1652f4be7a2a0e092a78af7c637f85a505
Aitor <acalvo049@ehu.eus>
2023-05-07 23:57:11 +0200

    add comments to dependencies.jl

--- dependencies.jl
+++ dependencies.jl
@@ -2,10 +2,24 @@
 
 using Pkg
 
+# file management
 Pkg.add("Glob") 
+
+# I/O
 Pkg.add("DelimitedFiles")
+
+# vector-matrix operations
 Pkg.add("LinearAlgebra")
+
+# basis generation
 Pkg.add("Combinatorics")
+
+# spin Clebsch-Gordan coefficients
 Pkg.add("PartialWaveFunctions")
+
+# interpolations for the spectral function
 Pkg.add("Interpolations")
+
+# parallelization
+Pkg.add("Distributed")
 
