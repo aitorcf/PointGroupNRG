c6a42517348654b68c2850b9b0e0b0e0ecf468ef
Aitor <acalvo049@ehu.eus>
2023-05-07 09:16:55 +0200

    rename InstallDependencies.jl to dependencies.jl

---
+++ dependencies.jl
@@ -0,0 +1,11 @@
+#!/usr/bin/env julia
+
+using Pkg
+
+Pkg.add("Glob") 
+Pkg.add("DelimitedFiles")
+Pkg.add("LinearAlgebra")
+Pkg.add("Combinatorics")
+Pkg.add("PartialWaveFunctions")
+Pkg.add("Interpolations")
+
