7a4fac4ee01610e23fe6135d616ca128e29b36ad
Aitor <acalvo049@ehu.eus>
2023-05-09 10:59:15 +0200

    change order of parameters

--- multiplets_TEMPLATE.jl
+++ multiplets_TEMPLATE.jl
@@ -1,17 +1,17 @@
 #!/usr/bin/env julia 
 
 # load multiplet module
-moduledir = "/path/to/modules/directory"
+moduledir = "/path/to/module/directory"
 include( "$(moduledir)/multiplets.jl" )
+
+# path to clebsch-gordan coefficients for the chosen orbital
+cg_path = "path/to/clebsch/gordan/coefficients"
 
 # orbital irrep for which to compute the multiplets.
 orbital = "orbital_irrep"
 
 # directory where multiplet folder will be stored
 multiplets_path =  "path/to/multiplet/folder"
-
-# path to clebsch-gordan coefficients for the chosen orbital
-cg_path = "path/to/clebsch/gordan/coefficients"
 
 # Compute multiplet states.
 compute_asymstates_allN( orbital , cg_path , multiplets_path ; verbose=true )
