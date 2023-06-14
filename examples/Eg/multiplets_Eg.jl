0e795f04b7bde9e1c2ae7a6af2c6ffc800e042cf
Aitor <acalvo049@ehu.eus>
2023-06-14 10:47:49 +0200

    example Eg ready

--- examples/Eg/multiplets_Eg.jl
+++ examples/Eg/multiplets_Eg.jl
@@ -1,17 +1,17 @@
 #!/usr/bin/env julia 
 
 # load multiplet module
-moduledir = "/home/aitor/Bulegoa/PointGroupNRG/modules"
+moduledir = "../../modules"
 include( "$(moduledir)/multiplets.jl" )
 
 # path to clebsch-gordan coefficients for the chosen orbital
-cg_path = "/home/aitor/Bulegoa/PointGroupNRG/examples/clebschgordan"
+cg_path = "../clebschgordan"
 
 # orbital irrep for which to compute the multiplets.
 orbital = "Eg"
 
 # directory where multiplet folder will be stored
-multiplets_path =  "./multiplets"
+multiplets_path =  "multiplets"
 
 # Compute multiplet states.
-compute_asymstates_allN( orbital , cg_path , multiplets_path ; verbose=true )
+compute_multiplets( orbital , cg_path , multiplets_path ; verbose=true )
--- examples/Eg/nrg_Eg.jl
+++ examples/Eg/nrg_Eg.jl
@@ -1,54 +1,47 @@
 #!/usr/bin/env julia
 
 # load modules
-moduledir = "/home/aitor/Bulegoa/PointGroupNRG/modules"
+moduledir = "../../modules"
 include( "$(moduledir)/modules.jl" )
 
-# name given to the system
-label = "Eg"
-
-# numerical parameters
-L = 5.0
-cutoff_type = "multiplet"
-cutoff_magnitude = 300
-iterations = 60
-
-# z averaging 
-Nz = 4
-Z = generate_Z(Nz)
-
-# clebsch-gordan and multiplet directories
-cg_o_dir = "/home/aitor/Bulegoa/PointGroupNRG/examples/clebschgordan"
-#cg_o_dir = "/home/aitor/Bulegoa/ClebschGordan/Oh/cg_symbolic
+# input for run=="multiplets"
+cg_o_dir = "../clebschgordan"
 multiplets_dir = "multiplets"
+atom_config = Dict{String,Int64}(
+    "Eg" => 1
+)
 identityrep = "A1g"
 
-# configuration of impurity and shells
-imp_config = Dict{String,Int64}( "Eg" => 1 )
-shell_config = imp_config
-
-# parameters of the hamiltonian
+# additional input for run=="spectrum"
 epsilon_symparams = Dict{String,Vector{ComplexF64}}(
     "Eg" => [-0.1]
 )
-u_symparams = Dict{ Tuple{String,Float64} , Matrix{ComplexF64} }(
-    ("Eg",0.0)  => [0.6;;],
-    ("A1g",0.0) => [0.5;;],
-    ("A2g",1.0) => [0.1;;]
+u_symparams = Dict{Tuple{String,Float64},Matrix{ComplexF64}}(
+    ("Eg",0.0) => [0.3;;],
+    ("A1g",0.0) => [0.2;;],
+    ("A2g",1.0) => [0.05;;]
 )
-hop_symparams = Dict{ String , Matrix{ComplexF64} }(
+
+# additional input for run=="thermo" (and run=="spectral")
+label = "Eg"
+cutoff_type = "multiplet"
+cutoff_magnitude = 500
+L = 3.0
+iterations = 60
+shell_config = atom_config
+hop_symparams = Dict{String,Matrix{ComplexF64}}(
     "Eg" => [0.1;;]
 )
 
 # choose what to calculate
-run = "thermo"
+run = "spectral"
 
 if run=="multiplets"
 
-    multiplets_2part( 
+    multiplets_2particles( 
         cg_o_dir ,
         multiplets_dir ,
-        imp_config ,
+        atom_config ,
         identityrep
     )
 
@@ -57,7 +50,7 @@ elseif run=="spectrum"
     impurity_spectrum( 
         cg_o_dir ,
         multiplets_dir ,
-        imp_config ,
+        atom_config ,
         identityrep ,
         epsilon_symparams ,
         u_symparams
@@ -76,7 +69,7 @@ elseif run=="thermo"
             cutoff_magnitude ,
             cg_o_dir ,
             multiplets_dir ,
-            imp_config ,
+            atom_config ,
             shell_config ,
             identityrep ,
             epsilon_symparams ,
@@ -99,7 +92,7 @@ elseif run=="spectral"
         cutoff_magnitude ,
         cg_o_dir ,
         multiplets_dir ,
-        imp_config ,
+        atom_config ,
         shell_config ,
         identityrep ,
         epsilon_symparams ,
@@ -107,33 +100,5 @@ elseif run=="spectral"
         hop_symparams ;
         spectral=true
     )
-
-elseif run=="thermozavg"
-
-
-    for calculation in ["CLEAN","IMP"],
-        z in Z
-
-        nrg_full( 
-            label ,
-            calculation ,
-            L ,
-            iterations ,
-            cutoff_type ,
-            cutoff_magnitude ,
-            cg_o_dir ,
-            multiplets_dir ,
-            imp_config ,
-            shell_config ,
-            identityrep ,
-            epsilon_symparams ,
-            u_symparams ,
-            hop_symparams ;
-            z=z
-        )
-
-    end
-
-    zavg_thermo( label , Z )
 
 end
