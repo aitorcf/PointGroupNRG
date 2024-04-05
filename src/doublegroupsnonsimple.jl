6f46b49f42afec00d51443b26c35f68c85c11f7b
Aitor <acalvo049@ehu.eus>
2024-04-05 17:51:37 +0200

    ok double-G, ok multiplets double-G nonsimple

--- src/doublegroupsnonsimple.jl
+++ src/doublegroupsnonsimple.jl
@@ -1,165 +1,178 @@
-function multiplets_2particles_doublegroups( 
-            cg_o_dir::String ,
-            multiplets_path::String ,
-            impurity_config::Dict{String,Int64} ,
-            identityrep::String )
+function cg_orbital_nonsimple( I_1::String , I_2::String , path ; verbose=false )
 
-    atom_orbital_irreps = collect(keys(impurity_config))
+    # STRING version. 
+    # Given two orbital irreps I_1 and I_2, it searches in path 
+    # for the file containing CG information and returns it in 
+    # the form of a dictionary:
+    #
+    #           cg[I_1,i_1,I_2,i_2,I_3,i_3,r_3] = ( I_1 , i_1 ; I_2 , i_2 | I_3 , i_3 , r_3 )
+    #
+    #
+    cg::Dict{ Tuple{String,Int64,String,Int64,String,Int64,Int64} , ComplexF64 } = 
+        Dict{ Tuple{String,Int64,String,Int64,String,Int64,Int64} , ComplexF64 }()
 
-    # orbital symmetry
-    (oirreps,
-     oirreps2indices,
-     oirreps2dimensions,
-     oindex2dimensions,
-     cg_o_fullmatint) = get_cg_o_info( cg_o_dir , atom_orbital_irreps )
+    file = [ x for x in readdir("$(path)/") 
+               if (occursin("$(I_1)x$(I_2)",x) || occursin("$(I_2)x$(I_1)",x)) ][1]
+    verbose && @show file 
 
-    # spin symmetry
-    cg_s_fullmatint = get_cg_s_fullmatint( 0 );
+    inverted = false
+    occursin("$(I_2)x$(I_1)",file) && (inverted=true)
 
-    
-    symstates_atom_noint,basis_atom,multiplets_atom_noint,multiplets_a_atom_noint = 
-        get_symstates_basis_multiplets_doublegroups( 
-                impurity_config,
-                oirreps2dimensions,
-                identityrep,
-                multiplets_path,
-                cg_o_dir ;
-                verbose=true )
-    multiplets_atom_twopart = filter( 
-            m->m[1]==2 ,
-            multiplets_atom_noint )
+    I_3::String = "a"
+    r_3::Int64 = 1
+    for line in readlines( "$(path)/$(file)" ) 
+        line=="" && continue
+        sline = split(strip(line)," ")
+        I_3,r_3 = length(sline)==3 ? (sline[2],parse(Int64,sline[3])) : (I_3,r_3)
+        length(sline)==3 && continue
+        sline = [sline[2:3]...,sline[5],reduce(*,sline[8:end])]
+        sline[end] = reduce( * , replace.( sline[end] , "I"=>"im" ) )
+        sline = map( x -> eval(Meta.parse(x)) , sline )
+        if ! inverted
+            push!( cg , (I_1,sline[1]::Int64,I_2,sline[2]::Int64,I_3,sline[3]::Int64,r_3)=>sline[4] )
+        else
+            push!( cg , (I_1,sline[2]::Int64,I_2,sline[1]::Int64,I_3,sline[3]::Int64,r_3)=>sline[4] )
+        end
+    end
+    return cg
+end
 
-    println( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" )
-    println( "TWO-PARTICLE ATOMIC MULTIPLETS (with representative)" )
-    println( "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" )
-    for ma in multiplets_atom_twopart 
-        println( ma )
-        println( symstates_atom_noint[(ma[1:3]...,1,ma[3],ma[4])] )
+function cg_shortcircuit_nonsimple( CG_PATH , oirreps... ; verbose=false )
+    verbose && println( "recursion call" )
+    seeds::Vector{String} = collect( oirreps )
+    verbose && @show seeds
+    produced::Vector{String} = []
+    for seed_pair in with_replacement_combinations( seeds , 2 ) 
+        cg_1 = cg_orbital_nonsimple( seed_pair[1] , seed_pair[2] , CG_PATH ; verbose=verbose )
+        cg_2 = cg_orbital_nonsimple( seed_pair[2] , seed_pair[1] , CG_PATH ; verbose=verbose )
+        append!( produced , [k[5] for k in keys(cg_1)] )
+        append!( produced , [k[5] for k in keys(cg_2)] )
+        append!( produced , seed_pair )
+        append!( produced , reverse(seed_pair) )
+    end
+    produced = collect(Set(produced))
+    verbose && @show produced 
+    verbose && println()
+    if Set(produced)==Set(seeds)
+        return produced 
+    else 
+        return cg_shortcircuit_nonsimple( CG_PATH , produced... )
+    end
+end
+
+function get_cg_o_fulldict_nonsimple( oirreps , cg_path )
+    # Given a collection of orbital irreps 'oirreps', it searches in cg_path 
+    # for CG information and returns the coefficients for every possible
+    # combination (I_1,I_2) for I_1 and I_2 in oirreps:
+    #
+    #           cg[I_1,i_1,I_2,i_2,I_3,i_3] = ( I_1 , i_1 ; I_2 , i_2 | I_3 , i_3 )
+    #
+    cg_o_full = Dict{ Tuple{String,Int64,String,Int64,String,Int64,Int64} , ComplexF64 }()
+    for I1 in oirreps, I2 in oirreps 
+        merge!( cg_o_full , cg_orbital_nonsimple( I1 , I2 , cg_path ) )
+    end
+    return cg_o_full
+end
+
+function get_cg_o_fullmatint_nonsimple( 
+            cg_o_fulldict::Dict{Tuple{String,Int64,String,Int64,String,Int64,Int64},ComplexF64} , 
+            oirreps::Vector{String} )::Dict{ NTuple{3,Int64} , Array{ComplexF64,4} } 
+    # 
+    # Given a CG coefficient dictionary in the form 
+    #
+    #           cg[I_1,i_1,I_2,i_2,I_3,i_3] = ( I_1 , i_1 ; I_2 , i_2 | I_3 , i_3 ),
+    #
+    # where I_1, I_2 and I_3 are STRINGS, it transforms it into a dictionary of matrices 
+    #
+    #           cg[I_1,I_2,I_3] = M(I_1,I_2,I_3), where
+    #           M(I_1,I_2;I_3)_{i_1,i_2,i_3}=(I_1,i_1;I_2,i_2|I_3,i_3),
+    #
+    # where all indices are INTEGERS.
+    #
+    oirreps2indices = Dict( o=>i for (i,o) in enumerate(oirreps) )
+    combs = Set( (k[1],k[3],k[5]) for k in keys(cg_o_fulldict) )
+
+    function find_max_r( I1::String , I2::String , I3::String , 
+                         cgodict::Dict{Tuple{String,Int64,String,Int64,String,Int64,Int64},ComplexF64} )
+        matching_keys = Set( k for k in keys(cgodict) if (k[1],k[3],k[5])==(I1,I2,I3) )
+        return maximum([k[end] for k in matching_keys])
+    end
+
+
+    cg_o_fullmat::Dict{Tuple{Int64,Int64,Int64},Array{ComplexF64,4}} = Dict() 
+    for (Is1,Is2,Is3) in combs
+        D1 = maximum(Set( k[2] for k in keys(cg_o_fulldict) if k[1]==Is1 ))
+        D2 = maximum(Set( k[4] for k in keys(cg_o_fulldict) if k[3]==Is2 ))
+        D3 = maximum(Set( k[6] for k in keys(cg_o_fulldict) if k[5]==Is3 ))
+        R3 = find_max_r(Is1,Is2,Is3,cg_o_fulldict)
+        I1 = oirreps2indices[Is1]
+        I2 = oirreps2indices[Is2]
+        I3 = oirreps2indices[Is3]
+        push!( cg_o_fullmat , 
+               (I1,I2,I3) => 
+               ComplexF64[ get(cg_o_fulldict,(Is1,i1,Is2,i2,Is3,i3),zero(ComplexF64))
+                           for r3=1:R3, i1=1:D1, i2=1:D2, i3=1:D3 ]
+         )
+    end
+    return cg_o_fullmat 
+end
+
+function get_cg_o_info_nonsimple( 
+            cg_o_dir::String , 
+            atom_orbital_irreps::Vector{String} ;
+            verbose=false )
+
+    if verbose
+        println( "GETTING ORBITAL CG INFORMATION" )
         println()
     end
-end
 
-function impurity_spectrum_doublegroups( 
-            cg_o_dir::String ,
-            multiplets_dir::String ,
-            impurity_config::Dict{String,Int64} ,
-            identityrep::String ,
-            epsilon_symparams::Dict{ String , Vector{ComplexF64} } ,
-            u_symparams::Dict{ Tuple{String,Float64} , Matrix{ComplexF64} } ;
-            max_spin2::Int64=0 )
-
-    calculation = "IMP"
-
-    # orbital irreps present in the atom
-    atom_orbital_irreps::Vector{String} = collect(keys(impurity_config))
-
-    # hiztegia
-    hiztegia = Dict{String,Any}( o=>o for (o,_) in impurity_config )
-    merge!( hiztegia , Dict( "-"=>0.0 ) )
-
-    #   ==========================   #
-    #%% SYMMETRY-RELATED VARIABLES %%#
-    #   ==========================   #
-
-    # orbital symmetry
-    (oirreps::Vector{String},
-     oirreps2indices::Dict{String,Int64},
-     oirreps2dimensions::Dict{String,Int64},
-     oindex2dimensions::Vector{Int64},
-     cg_o_fullmatint::Dict{NTuple{3,Int64},Array{ComplexF64,3}}) = get_cg_o_info( cg_o_dir , atom_orbital_irreps )
-
-    # spin symmetry
-    cg_s_fullmatint::Dict{NTuple{3,Int64},Array{ComplexF64,3}} = get_cg_s_fullmatint( max_spin2 );
-
-    #   ===========   #
-    #%% ATOMIC PART %%#
-    #   ===========   #
-    println()
-    println( ":::::::::::::::::::" )
-    println( "--- ATOMIC PART ---" )
-    println( ":::::::::::::::::::" )
-    println()
-
-
-    #   ------------------------------- #
-    #%% symstates, basis and multiplets #
-    #   ------------------------------- #
-    symstates_atom_noint::Dict{Tuple{Int64,String,Float64,Int64,Float64,Int64},State} = Dict()
-    multiplets_atom_noint::Set{Tuple{Int64,String,Float64,Int64}} = Set()
-    multiplets_a_atom_noint::Set{Tuple{Int64,String,Float64,Int64}} = Set()
-    mult2index::Dict{Tuple{Int64,String,Float64,Int64},Int64} = Dict()
-    orbital_multiplets::Vector{Tuple{Int64,String,Float64,Int64}} = []
-    if calculation=="IMP"
-        symstates_atom_noint,
-        basis_atom,
-        multiplets_atom_noint,
-        multiplets_a_atom_noint = 
-            get_symstates_basis_multiplets_doublegroups( 
-                    impurity_config,
-                    oirreps2dimensions,
-                    identityrep,
-                    multiplets_dir,
-                    cg_o_dir ;
-                    verbose=true )
-        orbital_multiplets = ordered_multiplets(multiplets_atom_noint)
-        mult2index = Dict( m=>i for (i,m) in 
-                                                                    enumerate(orbital_multiplets))
-        multiplets_atom::Set{NTuple{4,Int64}} = multiplets2int( multiplets_atom_noint , 
-                                                                oirreps2indices )
-        multiplets_a_atom::Set{NTuple{4,Int64}} = multiplets2int( multiplets_a_atom_noint , 
-                                                                  oirreps2indices )
-    else 
-        multiplets_atom_noint = Set([(0,identityrep,0.0,1)]) 
-        multiplets_atom = multiplets2int(multiplets_atom_noint,
-                                                oirreps2indices)
-        multiplets_a_atom = multiplets_atom
+    # all needed orbital irreps, their indices and dimensions.
+    oirreps::Vector{String} = cg_shortcircuit_nonsimple( cg_o_dir , 
+                                                         atom_orbital_irreps )::Vector{String}
+    oirreps2indices::Dict{String,Int64} = Dict( o=>i for (i,o) in enumerate(oirreps) )
+    if verbose 
+        @show oirreps 
+        @show oirreps2indices
     end
 
-    #   ------------------------ #
-    #%% reduced pcg coefficients #
-    #   ------------------------ #
-    pcgred_atom::IntIrrepPCG = 
-        calculation=="CLEAN" ? 
-        IntIrrepPCG() :
-        get_pcgred( basis_atom ,
-                    symstates_atom_noint::ClearSymstateDict ,
-                    multiplets_atom::IntMultipletSet ,
-                    hiztegia ,
-                    oirreps2indices::Dict{String,Int64} ,
-                    cg_o_fullmatint::IntCG ,
-                    cg_s_fullmatint::IntCG ;
-                    verbose=false )::IntIrrepPCG
-        
-    #   ------------- #
-    #%% impurity atom #
-    #   ------------- #
-    if calculation=="IMP"
-
-        # operators
-        epsilon::Operator{typeof(basis_atom)} = epsilon_sym( symstates_atom_noint , epsilon_symparams ; verbose=false )
-        coulomb::Operator{typeof(basis_atom)} = u_sym( symstates_atom_noint , u_symparams ; verbose=false )
-
-        # hamiltonian 
-        H::Operator{typeof(basis_atom)} = epsilon + coulomb 
-
+    # clebsch-gordan matrix
+    cg_o_full::Dict{Tuple{String,Int64,String,Int64,String,Int64,Int64},ComplexF64} = get_cg_o_fulldict_nonsimple( oirreps , cg_o_dir )
+    cg_o_fullmatint::Dict{NTuple{3,Int64},Array{ComplexF64,3}} = get_cg_o_fullmatint_nonsimple( cg_o_full , oirreps )
+    if verbose 
+        println( "Orbital CG matrix:" )
+        print_dict( cg_o_fullmatint ) 
+        println()
     end
 
-    #   -----   #
-    #%% irreu %%#
-    #   -----   #
-    irrEU_clear::Dict{ Tuple{Int64,String,Float64} , Tuple{Vector{Float64},Matrix{ComplexF64}} } = Dict()
-    if calculation=="IMP"
-        irrEU_clear = 
-            get_irrEU_initial(symstates_atom_noint,H;verbose=true)::Dict{ Tuple{Int64,String,Float64} , Tuple{Vector{Float64},Matrix{ComplexF64}} }
-    elseif calculation=="CLEAN" 
-        irrEU_clear = 
-            get_irrEU_initial(identityrep,oirreps2indices)::Dict{ Tuple{Int64,String,Float64} , Tuple{Vector{Float64},Matrix{ComplexF64}} }
+
+    # orbital dimensions
+    oirreps2dimensions::Dict{String,Int64} = Dict()
+    for (ostring,oindex) in oirreps2indices
+        for ((I1,I2,I3),IIImat) in cg_o_fullmatint 
+            valid = false
+            I1==oindex && (valid=true; i=1) 
+            I2==oindex && (valid=true; i=2) 
+            I3==oindex && (valid=true; i=3) 
+            valid || continue
+            oirreps2dimensions[ostring] = size(IIImat)[i+1]
+        end
     end
-    print_spectrum( irrEU_clear )
+
+    #oirreps2dimensions = Dict( "Eg" => 2 ,
+    #                           "A1g"=> 1 ,
+    #                           "A2g"=> 1 )
+    oindex2dimensions::Vector{Int64} = collect( oirreps2dimensions[I] for I in oirreps )
+
+    return (oirreps,
+            oirreps2indices,
+            oirreps2dimensions,
+            oindex2dimensions,
+            cg_o_fullmatint)
 
 end
 
-function nrg_full_doublegroups( 
+function nrg_full_doublegroups_nonsimple( 
             label::String ,
             calculation::String ,
             L::Float64 ,
@@ -252,6 +265,7 @@ function nrg_full_doublegroups(
      oirreps2dimensions::Dict{String,Int64},
      oindex2dimensions::Vector{Int64},
      cg_o_fullmatint::Dict{NTuple{3,Int64},Array{ComplexF64,3}}) = get_cg_o_info( cg_o_dir , atom_orbital_irreps )
+    x=y
 
     # for dmnrg
     shell_dimension = reduce( * , [4^(oirreps2dimensions[I]*R) for (I,R) in shell_config] )
@@ -1035,85 +1049,3 @@ function nrg_full_doublegroups(
     println()
     println( "END OF FULL NRG CALCULATION WITH z=$(z)" )
 end
-
-# ---------------- #
-# BASIS AND STATES #
-# ---------------- #
-
-function get_atomic_states_doublegroups( 
-            external_label::Int64 ,
-            ostring::String , 
-            oirreps2dimensions::Dict{String,Int64} )::Vector{Tuple{Int64,String,Int64,String}}
-
-    root = ( external_label , ostring )
-    statetuples = Tuple{Int64,String,Int64,String}[]
-
-    for orbital in 1:oirreps2dimensions[ostring]
-
-        push!( statetuples , (root...,orbital,"-") )
-
-    end
-
-    return statetuples
-end
-
-function get_symstates_basis_multiplets_doublegroups( 
-            atom_config::Dict{String,Int64},
-            oirreps2dimensions::Dict{String,Int64} ,
-            identityrep::String ,
-            asym_dir::String ,
-            cg_o_dir::String ;
-            verbose=true )
-
-    # hiztegia
-    hiztegia::Dict{String,Any} = Dict( o=>o for (o,_) in atom_config )
-    merge!( hiztegia , Dict( "-"=>0.0 ) )
-
-    # symstates
-    separate_symstates = []
-    for (oirrep,multiplicity) in atom_config
-        for m in 1:multiplicity
-            onemult_states = get_atomic_states_doublegroups(m,oirrep,oirreps2dimensions)
-            onemult_hilbert = HilbertSpace( onemult_states )
-            onemult_symstates = oneirrep_symstates( 
-                                    onemult_hilbert ,
-                                    hiztegia ,
-                                    identityrep ,
-                                    "$(asym_dir)/$(oirrep)/" )
-            push!( separate_symstates , onemult_symstates )
-        end
-    end
-    symstates = separate_symstates[1]
-    for symstates_new in separate_symstates[2:end]
-        symstates = cg_reduce_product_states(
-                        symstates , 
-                        symstates_new ,
-                        cg_o_dir )
-    end
-    
-    # basis 
-    basis::CanonicalBasis = collect(values(symstates))[1].basis 
-
-    # multiplets 
-    multiplets::Set{Tuple{Int64,String,Float64,Int64}} = get_multiplets( symstates )
-    multiplets_a::Set{Tuple{Int64,String,Float64,Int64}} = Set( m for m in multiplets if m[1]==1 )
-
-    # printing
-    if verbose 
-        println( "BASIS" )                                               
-        println( basis )
-        println()
-        println( "SYMSTATES" )
-        print_symstates_ordered( symstates )
-        println()
-        println( "MULTIPLETS" )
-        print_multiplets_Nordered( multiplets )
-        println()
-        println( "ONE-ELECTRON MULTIPLETS" )
-        print_multiplets_Nordered( multiplets_a )
-        println()
-    end
-
-    return (symstates,basis,multiplets,multiplets_a)
-end
-
